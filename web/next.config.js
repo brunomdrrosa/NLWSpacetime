afa4b9350f98887c685be25fc44cb3cc51db1947
Bruno Machado <brunomdr46@gmail.com>
2023-05-20 23:19:58 -0300

    Aula 05 - O próximo nível

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['avatars.githubusercontent.com'],
+    domains: [
+      'avatars.githubusercontent.com',
+      'localhost',
+      '192.168.3.7:3333',
+      '192.168.3.7:19000',
+      '192.168.3.7',
+    ],
   },
 }
 
